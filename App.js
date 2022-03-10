fbc50bdc20bb597067f1b5296e092ecfdb73df42
Guilherme Souza <gss214@hotmail.com>
2022-03-10 01:45:33 -0300

    testing google maps api

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Parkit</Text>
+      <MapView style={{width: 400, height: 400}}
+        initialRegion={{
+          latitude: -15.88621292923914,
+          longitude: -48.01274074440105,
+          latitudeDelta: 0.92,
+          longitudeDelta: 0.0421
+        }}
+      ></MapView>
     </View>
   );
 }
