0d9559f2ffb9668366a4f22b049148bebf99b32f
Guilherme Souza <gss214@hotmail.com>
2022-04-06 23:45:00 -0300

    fix login and added user uid in users collection

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
-import { getFirestore } from "@firebase/firestore"
+import { initializeFirestore } from "@firebase/firestore"
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -15,5 +15,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(db);
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
 export const storage = getStorage(app)
