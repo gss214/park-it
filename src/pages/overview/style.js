2d8fbd429e4ca460ddc5a621206a58a802c03995
Pedro Maschio <30641706+pedro-maschio@users.noreply.github.com>
2022-04-11 22:54:56 -0300

    Finishing overview screen and imporving profile screen (#11)

--- src/pages/overview/style.js
+++ src/pages/overview/style.js
@@ -7,10 +7,42 @@ const styles = StyleSheet.create({
     alignItems:'center'
   },
   mapView: {
-    width: 600,
+    width: "100%",
     height: 200,
     position: 'absolute',
     top: 0
+  },
+  overviewContainer: {
+    width: "100%",
+    textAlign: "left",
+    paddingLeft: 20,
+  },
+  overviewItem: {
+    width: "100%",
+    fontSize: 16
+  },
+  parkingTitle: {
+    fontSize: 24,
+    fontWeight: '800',
+    paddingBottom: 10
+  },
+
+  ratingView: {
+    flexDirection:'row'
+  },
+  rating: {
+    paddingTop: 15,
+    paddingRight: 10
+  },
+  barRating: {
+    paddingTop: 10,
+    paddingRight: 10
+  },
+  starCount: {
+    paddingTop: 15,
+  },
+  isPrivate: {
+    paddingTop: 5
   }
 });
 
